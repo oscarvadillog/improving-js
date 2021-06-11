03ab19c7ca2f75b96a4ed87ea6667743ed92e67e
Oscar <oscar.vadillog@gmail.com>
2021-06-11 09:41:01 +0200

    Update server.js

--- fetch/server.js
+++ fetch/server.js
@@ -5,9 +5,9 @@ http.createServer((req, res) => {
     console.log(`New incoming HTTP request - Method: ${chalk.green(req.method)} - URL: ${chalk.yellow(req.url)}`);
 
     // Enable CORS
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Request-Method', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
     res.setHeader('Access-Control-Allow-Headers', '*');
 
     // Response
